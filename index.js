e61936d49054d5900f905fb325805ec0ab759ae9
avali4907 <avali4907@outlook.com>
2021-11-03 20:00:38 -0700

    reorganized file

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+// Imports
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require("@octokit/rest");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+// Globals
+const nameToGreet = core.getInput('who-to-greet'); // `who-to-greet` input defined in action metadata file
+const payload = JSON.stringify(github.context.payload, undefined, 2) // Get the JSON webhook payload for the event that triggered the workflow
+
+function main() {
+  try {
+    console.log(`Hello ${nameToGreet}!`);
+
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main()
