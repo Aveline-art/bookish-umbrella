53e5005ec316fc103a4bce4c3cbe193aaed15c77
avali4907 <avali4907@outlook.com>
2021-11-06 18:24:36 -0700

    made tests

--- unittest.js
+++ unittest.js
@@ -1,8 +1,30 @@
-const eval = require('./repl');
+const main = require('./repl');
 
-const s1 = 'dependency or good first issue'
-const s2 = '(good first issue and dependency) or (good second issue and backend'
-const s3 = 'not (good first issue and dependency)'
-const s4 = 'not good first issue'
+const s1 = 'dependency and not bug'
+const s2 = '(bug and dependency) or (enhancement and invalid)'
+const s3 = 'not (bug and dependency)'
+const s4 = 'not bug'
+const s5 = 'bug and dependency or enhancement'
+const s6 = `"good first issue" and ('invalid' or 'help wanted')`
 
-console.log(eval(1, 2))
+const a1 = ['dependency', 'bug']
+const a2 = ['dependency', 'invalid']
+const a3 = ['enhancement']
+const a4 = ['enhancement', 'invalid']
+
+function testInterpreter(s, a) {
+    const interpreter = new main.Interpreter(s, a)
+    const tokens = interpreter.tokenize()
+    console.log(tokens)
+}
+
+function tests() {
+    testInterpreter(s1, a1)
+    testInterpreter(s2, a1)
+    testInterpreter(s3, a1)
+    testInterpreter(s4, a1)
+    testInterpreter(s5, a1)
+    testInterpreter(s6, a1)
+}
+
+tests()
