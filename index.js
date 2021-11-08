98f4326130b45f423164d86f8b80a97da95fae39
avali4907 <avali4907@outlook.com>
2021-11-07 19:46:56 -0800

    added new fields to yml file

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 // Imports
 const core = require('@actions/core');
 const github = require('@actions/github');
+const repl = require('./repl');
 
 // Globals
 //const myToken = core.getInput('myToken');
+// TODO, find out how to get the token in the yml file
 //const octokit = github.getOctokit(myToken)
-const nameToGreet = core.getInput('who-to-greet');
+const labels = core.getInput('labels')
 const payload = JSON.stringify(github.context.payload, undefined, 2)
 const eventName = JSON.stringify(github.context.eventName)
 
+
+// TODO, if no label string is provided, the check is skipped
 function main() {
   try {
     if (eventName == 'issues') {
+      const issueLabels = payload.issue.labels
+      if (repl.analyze(labels, issueLabels)) {
+        // API call
+      }
       // Do something
     } else if (eventName == 'pull_request') {
+      const prLabels = payload.issue.labels
+      if (repl.analyze(labels, prLabels)) {
+        // API call
+      }
       // Do something else
     }
 
