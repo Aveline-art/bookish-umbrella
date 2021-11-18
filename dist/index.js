b335050d5a6a742ea758f6d4533fe361f3a3a6b0
avali4907 <avali4907@outlook.com>
2021-11-18 15:09:29 -0800

    testing

--- dist/index.js
+++ dist/index.js
@@ -9113,7 +9113,7 @@ function parseMessage(string) {
     const path = string.substring(4).trim()
     console.log(path)
     console.log('Current directory: ' + process.cwd());
-    fs.readdirSync(process.cwd(), (err, files) => {
+    fs.readdir('./', (err, files) => {
       files.forEach(file => {
         console.log(file);
       });
